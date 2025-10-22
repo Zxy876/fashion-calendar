f92c73b7bb51390f805fec1e3cf34cf4bfb27db9
Zxy876 <xiangyu_0514@qq.com>
2025-10-22 12:08:25 +0800

    仿照已选好的日历模版，完成首页UI设计

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CalendarPage from './components/CalendarPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CalendarPage />
+    </div>
+  );
+}
+
+export default App;
