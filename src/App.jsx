09cb400ffc3b0bd77a29c5d8389fe64a10f196a4
Zxy876 <xiangyu_0514@qq.com>
2025-10-25 07:45:57 +0800

    改造app路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CalendarPage from './components/CalendarPage';
-import DateDetailPage from './components/DateDetailPage';
+import EventDetailPage from './components/EventDetailPage';
 import './App.css';
-
+ 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<CalendarPage />} />
-          <Route path="/date/:date" element={<DateDetailPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<CalendarPage />} />
+        <Route path="/day/:date" element={<EventDetailPage />} />
+      </Routes>
     </Router>
   );
 }
-
+ 
 export default App;
